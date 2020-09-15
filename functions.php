131b75af7ebd959e05162b696e3334873a570d73
moitorrijos <juanmtorrijos@gmail.com>
2020-09-14 23:19:28 -0500

    Arreglar estilos de iconos de ponentes.

--- functions.php
+++ functions.php
@@ -193,7 +193,7 @@ add_action( 'widgets_init', 'communityconf2020_widgets_init' );
  */
 function communityconf2020_scripts() {
 	wp_enqueue_style( 'communityconf2020-fonts', "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Saira:wght@400;600;800&display=swap", array(), '002', 'all' );
-	wp_enqueue_style( 'communityconf2020-style', get_stylesheet_uri(), array(), '06' );
+	wp_enqueue_style( 'communityconf2020-style', get_stylesheet_uri(), array(), '07' );
 	wp_style_add_data( 'communityconf2020-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'communityconf2020-navigation', get_template_directory_uri() . '/js/navigation.js', array(), COMMCONF_VERSION, true );
@@ -210,6 +210,7 @@ function communityconf2020_scripts() {
 		wpcf7_enqueue_scripts();
 	}
 }
+
 add_action( 'wp_enqueue_scripts', 'communityconf2020_scripts' );
 
 /**
